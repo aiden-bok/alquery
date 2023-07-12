aaa064a88c99dcd295514991323e39a2b479aa5a
Aiden <bokjaeoh@gmail.com>
2023-07-12 09:50:35 +0900

    Add test for `parseLimit` method

---
+++ test/parseLimit.test.js
@@ -0,0 +1,59 @@
+import alquery from '../src/index.js'
+
+// undefined
+test("parseLimit() returns ''", () => {
+  const call = alquery.parseLimit()
+  expect(call).toBe('')
+})
+
+// null
+test("parseLimit(null) returns ''", () => {
+  const limit = null
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe('')
+})
+
+// empty string
+test("parseLimit('') returns ''", () => {
+  const limit = ''
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe('')
+})
+
+// string
+test("parseLimit('age') returns ''", () => {
+  const limit = 'age'
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe('')
+})
+
+test("parseLimit('5') returns ' LIMIT 5'", () => {
+  const limit = '5'
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe(' LIMIT 5')
+})
+
+test("parseLimit('-5') returns ''", () => {
+  const limit = '-5'
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe('')
+})
+
+// number
+test("parseLimit(0) returns ''", () => {
+  const limit = 0
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe('')
+})
+
+test("parseLimit(5) returns ' LIMIT 5'", () => {
+  const limit = 5
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe(' LIMIT 5')
+})
+
+test("parseLimit(-5) returns ''", () => {
+  const limit = -5
+  const call = alquery.parseLimit(limit)
+  expect(call).toBe('')
+})
